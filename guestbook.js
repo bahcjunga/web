0c08e3199df85ffb3ad19213df5f7fed05a65560
bahcjunga <bahcjunga@gmail.com>
2024-06-11 21:53:28 +0900

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,29 +1,27 @@
 
-      document.getElementById('guestbook-form').addEventListener('submit', function(event) {
-        event.preventDefault();
+   
+        document.getElementById('guestbook-form').addEventListener('submit', function(event) {
+            event.preventDefault();
 
-        var name = document.getElementById('name').value;
-        var message = document.getElementById('message').value;
+            var name = document.getElementById('name').value;
+            var message = document.getElementById('message').value;
 
-        fetch('https://script.google.com/macros/s/AKfycbytZi4_cCCzAVomaQyXy_MFh4B33K9Gme3-gIpAODh838U8kzobjh8di4ELneiestmx/exec', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-          body: 'name=' + encodeURIComponent(name) + '&message=' + encodeURIComponent(message)
-        })
-        .then(response => response.text())
-        .then(data => {
-          if (data === 'Success') {
-            var entry = document.createElement('div');
-            entry.classList.add('entry');
-            entry.innerHTML = `<strong>${name}</strong><p>${message}</p>`;
-            document.getElementById('guestbook-entries').appendChild(entry);
-            document.getElementById('guestbook-form').reset();
-          } else {
-            console.error('Error:', data);
-          }
-        })
-        .catch(error => console.error('Error:', error));
-      });
+            fetch('https://script.google.com/macros/s/AKfycbxLXe3T1CRbyE1KhyxofOYfI-RHVwHpScZ8MKQgXjxLKQfliX88RbXAK3EaNtiaK21J/exec', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: 'name=' + encodeURIComponent(name) + '&message=' + encodeURIComponent(message)
+            })
+            .then(response => response.text())
+            .then(data => {
+                if (data === 'Success') {
+                    alert('Message submitted successfully!');
+                    document.getElementById('guestbook-form').reset();
+                } else {
+                    console.error('Error:', data);
+                }
+            })
+            .catch(error => console.error('Error:', error));
+        });
    
